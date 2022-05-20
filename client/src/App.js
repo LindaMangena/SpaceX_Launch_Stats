76fa9135ec777f1e39c7e23d87be819d3f06cf15
LindaMangena <lindajazz42@gmail.com>
2022-05-20 10:33:53 +0200

    apolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import logo from "./logo.png";
+import { ApolloClient } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import "./App.css";
+
+
+const client = new ApolloClient({
+
+  uri : 'http://localhost:8080/graphql'
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      <img src={logo} alt="SpaceX" 
-      style={{width : 200 , display : 'block' , margin : 'auto'}}/>
+      <img
+        src={logo}
+        alt="SpaceX"
+        style={{ width: 200, display: "block", margin: "auto" }}
+      />
     </div>
+    </ApolloProvider >
   );
 }
 
